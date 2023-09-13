50c03a46fbc2b84dec242aecc0f22a6dfadf389d
Malin Rudin <ma3732ru-s@student.lu.se>
2023-09-13 10:44:37 +0200

    reset foo.js

--- base/server/src/main/resources/webassets/foo/foo.js
+++ base/server/src/main/resources/webassets/foo/foo.js
@@ -21,33 +21,7 @@ base.fooController = function() {
         this.render = function(template) {
             this.update(template.content.querySelector('tr'));
             const clone = document.importNode(template.content, true);
-            this.fooRow = clone.querySelector('tr');
-
-const buttons = tds[2].querySelectorAll('button');
-
-// This function is called with the response from the server
-const updateFoo = function(newTotal) {
-    viewModel.foo.total = newTotal;
-    viewModel.update(viewModel.fooRow);
-};
-
-// Decrement button onclick function
-buttons[0].onclick = function(event) {
-    if (viewModel.foo.total == 1) {
-        base.rest.deleteFoo(viewModel.foo.id).then(function() {
-            viewModel.fooRow.parentElement.removeChild(viewModel.fooRow);
-        });
-    } else {
-        base.rest.updateFoo(viewModel.foo.id, viewModel.foo.total - 1).then(updateFoo);
-    }
-};
-// Increment button onclick function
-buttons[2].onclick = function() {
-    
-        base.rest.updateFoo(viewModel.foo.id, viewModel.foo.total + 1).then(updateFoo);
-    
-    // TODO: complete this (look at the decrement button).
-};
+            // TODO: Add stuff from lab 2 end-2-end task here
             template.parentElement.appendChild(clone);
         };
         // Update a single table row to display a foo
@@ -57,7 +31,6 @@ buttons[2].onclick = function() {
             const d = viewModel.foo.createdDate;
             tds[1].textContent = d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
             // TODO: Add stuff from lab 1 here
-            const buttons =tds[2].querySelectorAll('button');
         };
     };
 
